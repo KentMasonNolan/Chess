3df5c1553a43f5272635109c7f19458974632c7f
Kent Nolan <kent.nolan@gmail.com>
2023-07-15 23:02:35 +1200

    Setup for king and checking if piece is pinned, in check or checkmate

---
+++ TestBoard/GameState.java
@@ -0,0 +1,15 @@
+package TestBoard;
+
+public class GameState {
+    private ChessTile[][] chessboard;
+    private String currentPlayer;
+    private boolean isCheck;
+    private boolean isCheckmate;
+
+
+    public boolean isKingInCheck(String kingColor) {
+        //check if king is in check
+        return false;
+    }
+}
+
---
+++ TestBoard/King.java
@@ -0,0 +1,17 @@
+package TestBoard;
+
+public class King extends Piece{
+    public King(String color, String type) {
+        super(color, type);
+    }
+
+    @Override
+    public boolean isValidMove(int sourceRow, int sourceCol, int destRow, int destCol, ChessTile[][] chessboard) {
+        return false;
+    }
+
+    @Override
+    public boolean isValidCapture(int sourceRow, int sourceCol, int destRow, int destCol, ChessTile[][] chessboard) {
+        return false;
+    }
+}
