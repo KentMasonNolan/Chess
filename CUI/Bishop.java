039d495424445b4409ccd51305e58c3be25eb987
kent <kent.nolan@gmail.com>
2023-08-02 14:55:23 +1200

    Added bishop movement options.

--- CUI/Bishop.java
+++ CUI/Bishop.java
@@ -1,6 +1,6 @@
 package CUI;
 
-public class Bishop extends Piece{
+public class Bishop extends Piece {
     protected Bishop(String colour, int xLoc, int yLoc) {
         super(colour, "Bishop");
         this.xLoc = xLoc;
@@ -9,7 +9,11 @@ public class Bishop extends Piece{
 
     @Override
     public boolean isValidMove(int sourceRow, int sourceCol, int destRow, int destCol, ChessTile[][] chessboard) {
-        return false;
+        if (canMoveDiagonal(sourceRow, sourceCol, destRow, destCol, chessboard)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
