18ea131dac8e88bcd9e901fe248d5ff9a839f057
kent <kent.nolan@gmail.com>
2023-08-07 11:32:33 +1200

    Added knight ability to set capture squares based on movement.

--- CUI/Knight.java
+++ CUI/Knight.java
@@ -22,13 +22,76 @@ public class Knight extends Piece {
 
     @Override
     public void canCapture(int sourceRow, int sourceCol, ChessTile[][] chessboard) {
-        if (sourceRow - 2 > 0) {
-            if (sourceCol - 1 > 0) {
-                chessboard[sourceRow - 2][sourceCol - 1].setCanWhiteCapture(true);
+        if (colour.equals("white")) {
+
+            if (sourceRow - 2 >= 0) {
+                if (sourceCol - 1 > 0) {
+                    chessboard[sourceRow - 2][sourceCol - 1].setCanWhiteCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow - 2][sourceCol + 1].setCanWhiteCapture(true);
+                }
             }
-            if (sourceCol + 1 <= 7){
-                chessboard[sourceRow - 2][sourceCol + 1].setCanWhiteCapture(true);
-            } // todo keep adding more
+            if (sourceRow + 2 <= 7) {
+                if (sourceCol - 1 > 0) {
+                    chessboard[sourceRow + 2][sourceCol - 1].setCanWhiteCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow + 2][sourceCol + 1].setCanWhiteCapture(true);
+                }
+            }
+            if (sourceCol -2 >= 0){
+                if (sourceRow - 1 > 0) {
+                    chessboard[sourceRow - 1][sourceCol - 2].setCanWhiteCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow + 1][sourceCol - 2].setCanWhiteCapture(true);
+                }
+            }
+            if (sourceCol + 2 <= 7){
+                if (sourceRow - 1 > 0) {
+                    chessboard[sourceRow - 1][sourceCol + 2].setCanWhiteCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow + 1][sourceCol + 2].setCanWhiteCapture(true);
+                }
+            }
         }
+        else { // colour must be black
+
+            if (sourceRow - 2 >= 0) {
+                if (sourceCol - 1 > 0) {
+                    chessboard[sourceRow - 2][sourceCol - 1].setCanBlackCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow - 2][sourceCol + 1].setCanBlackCapture(true);
+                }
+            }
+            if (sourceRow + 2 <= 7) {
+                if (sourceCol - 1 > 0) {
+                    chessboard[sourceRow + 2][sourceCol - 1].setCanBlackCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow + 2][sourceCol + 1].setCanBlackCapture(true);
+                }
+            }
+            if (sourceCol -2 >= 0){
+                if (sourceRow - 1 > 0) {
+                    chessboard[sourceRow - 1][sourceCol - 2].setCanBlackCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow + 1][sourceCol - 2].setCanBlackCapture(true);
+                }
+            }
+            if (sourceCol + 2 <= 7){
+                if (sourceRow - 1 > 0) {
+                    chessboard[sourceRow - 1][sourceCol + 2].setCanBlackCapture(true);
+                }
+                if (sourceCol + 1 <= 7) {
+                    chessboard[sourceRow + 1][sourceCol + 2].setCanBlackCapture(true);
+                }
+            }
+        }
+
     }
 }
