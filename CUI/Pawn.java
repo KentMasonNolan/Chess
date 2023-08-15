13326ba8a83b7bef5398126f8ef59176974e71b8
Kent Nolan <kent.nolan@gmail.com>
2023-08-15 13:31:38 +1200

    Add the ability for pawns to promote

--- CUI/Pawn.java
+++ CUI/Pawn.java
@@ -1,6 +1,7 @@
 package CUI;
 
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Pawn extends Piece {
 
@@ -12,27 +13,33 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isValidMove(int sourceRow, int sourceCol, int destRow, int destCol, ChessTile[][] chessboard) {
-
-
         boolean isDestinationOccupied = chessboard[destRow][destCol].isTileFilled();
-        boolean isSquareAboveOccupied = chessboard[destRow - 1][destCol].isTileFilled(); // we need this because a pawn cannot jump a piece.
-        boolean isSquareBelowOccupied = chessboard[destRow + 1][destCol].isTileFilled(); // we need this because a pawn cannot jump a piece.
-        boolean isDestinationInBounds = false;
-
-        if ((destCol >= 0 && destCol <= 8) && (destRow >= 0 && destRow <= 8)) {
-            isDestinationInBounds = true;
-        }
+        boolean isSquareAboveOccupied = chessboard[destRow - 1][destCol].isTileFilled();
+        boolean isSquareBelowOccupied = chessboard[destRow + 1][destCol].isTileFilled();
+        boolean isDestinationInBounds = (destCol >= 0 && destCol <= 8) && (destRow >= 0 && destRow <= 8);
 
         if (Objects.equals(this.getColour(), "black")) {
             if (isFirstMove && (destRow == sourceRow - 1 || destRow == sourceRow - 2) && !isDestinationOccupied && isDestinationInBounds && !isSquareAboveOccupied) {
+                if (destRow == 0) {
+                    promotePawn(sourceRow, sourceCol, destRow, destCol, chessboard);
+                }
                 return true;
             } else if (!isFirstMove && destRow == sourceRow - 1 && !isDestinationOccupied && isDestinationInBounds) {
+                if (destRow == 0) {
+                    promotePawn(sourceRow, sourceCol, destRow, destCol, chessboard);
+                }
                 return true;
             }
-        } else /* colour will be white */ {
+        } else {
             if (isFirstMove && (destRow == sourceRow + 1 || destRow == sourceRow + 2) && !isDestinationOccupied && isDestinationInBounds && !isSquareBelowOccupied) {
+                if (destRow == 7) {
+                    promotePawn(sourceRow, sourceCol, destRow, destCol, chessboard);
+                }
                 return true;
             } else if (!isFirstMove && destRow == sourceRow + 1 && !isDestinationOccupied && isDestinationInBounds) {
+                if (destRow == 7) {
+                    promotePawn(sourceRow, sourceCol, destRow, destCol, chessboard);
+                }
                 return true;
             }
         }
@@ -81,6 +88,42 @@ public class Pawn extends Piece {
         }
     }
 
+
+
+
+
+    private void promotePawn(int sourceRow, int sourceCol, int destRow, int destCol, ChessTile[][] chessboard) {
+        // Prompt the user to choose a promotion piece (you can modify this part as needed)
+        System.out.println("Pawn promotion: Choose a promotion piece (Q/R/N/B):");
+        Scanner scanner = new Scanner(System.in);
+        String promotionChoice = scanner.nextLine().toUpperCase();
+
+        Piece promotionPiece;
+
+        // Create the appropriate piece based on the user's choice
+        switch (promotionChoice) {
+            case "Q":
+                promotionPiece = new Queen(this.getColour(), destRow, destCol);
+                break;
+            case "R":
+                promotionPiece = new Rook(this.getColour(), destRow, destCol);
+                break;
+            case "N":
+                promotionPiece = new Knight(this.getColour(), destRow, destCol);
+                break;
+            case "B":
+                promotionPiece = new Bishop(this.getColour(), destRow, destCol);
+                break;
+            default:
+                // Default to promoting to a Queen
+                promotionPiece = new Queen(this.getColour(), destRow, destCol);
+                break;
+        }
+
+        // Update the chessboard with the promoted piece
+        chessboard[destRow][destCol].setPiece(promotionPiece);
+        chessboard[sourceRow][sourceCol].removePiece();
+    }
 
     protected Pawn(String colour, int xLoc, int yLoc) {
         super(colour, "Pawn");
