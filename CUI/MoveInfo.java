58599bb6e06ac1d874235ec34cb243e673f372b7
Kent Nolan <kent.nolan@gmail.com>
2023-08-30 21:48:14 +1200

    changed move history to show moves in a better format

--- CUI/MoveInfo.java
+++ CUI/MoveInfo.java
@@ -9,11 +9,19 @@ class MoveInfo implements Serializable {
     private int destRow;
     private int destCol;
 
-    @Override
-    public String toString() {
-        return String.format("(%d, %d) -> (%d, %d)", sourceRow, sourceCol, destRow, destCol);
+// chatGPT wrote this but was less help that you would think
+    private String convertToChessCoordinate(int row, int col) {
+        char file = (char) ('h' -col);
+        char rank = (char) ('1' + row);
+        return String.valueOf(file) + rank;
     }
 
+    @Override
+    public String toString() {
+        String sourceCoordinate = convertToChessCoordinate(sourceRow, sourceCol);
+        String destCoordinate = convertToChessCoordinate(destRow, destCol);
+        return String.format("%s -> %s", sourceCoordinate, destCoordinate);
+    }
     public MoveInfo(int sourceRow, int sourceCol, int destRow, int destCol) {
         this.sourceRow = sourceRow;
         this.sourceCol = sourceCol;
